b743e26e288da913ce62e96f74f73079c2f37299
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2019-01-30 08:02:31 -0800

    Add validation to fuzz target
    This make sure new validation code is robust by itself.

--- fuzz/main.c
+++ fuzz/main.c
@@ -6,6 +6,10 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 	cgltf_options options = {0};
 	cgltf_data* data = NULL;
 	cgltf_result res = cgltf_parse(&options, Data, Size, &data);
-	if (res == cgltf_result_success) cgltf_free(data);
+	if (res == cgltf_result_success)
+	{
+		cgltf_validate(data);
+		cgltf_free(data);
+	}
 	return 0;
 }
